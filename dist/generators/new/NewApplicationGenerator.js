27e2dbaec95a55f0029e9073f9b2c5bd112c615b
Luka Klacar <l.klacar@vegaitsourcing.rs>
2018-10-01 12:41:21 +0200

    v1.0.8

--- dist/generators/new/NewApplicationGenerator.js
+++ dist/generators/new/NewApplicationGenerator.js
@@ -13,9 +13,7 @@ class NewApplicationGenerator extends BaseGenerator_1.default {
     generate(args) {
         chalk_animation_1.default.rainbow('Generating React Native application. Please wait...');
         execute_1.execute('react-native', ['init', args.appName, '--template', 'hike'])
-            .then(() => {
-            return execute_1.execute('node', [`${args.appName}/setup.js`]);
-        })
+            .then(() => execute_1.execute('node', [`${args.appName}/setup.js`]))
             .then(() => console.log(chalk_1.default.green('Application successfully generated.')))
             .catch(() => {
             console.log(chalk_1.default.red('Error while generating React Native application.'));
