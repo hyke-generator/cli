d846bd1f67b71d4eb34ef828ba7b9915bb51879f
Luka Klacar <lukaklacar@gmail.com>
2018-10-01 15:47:26 +0200

    #3 - Minor changes

--- src/generators/new/ComponentGenerator.ts
+++ src/generators/new/ComponentGenerator.ts
@@ -1,9 +1,10 @@
 import BaseGenerator from '../core/BaseGenerator';
 import Mustache from 'mustache';
 import { execute } from '../../util/execute';
-import { writeToFile } from '../../util/file';
+import { mkdir, writeToFile } from '../../util/file';
 import chalk from 'chalk';
 import * as path from 'path';
+import { getHikeDirectory } from '../../util/paths';
 
 const requireText = require('require-text');
 
@@ -16,17 +17,19 @@ export default class ComponentGenerator extends BaseGenerator<Args> {
         const directoryPath = './src/components';
         const componentName = args.componentName + '.tsx';
 
-        const pathToTemplate = path.join(__dirname, '..', '..', '..', 'templates/component.mustache');
+        const pathToTemplate = path.join(getHikeDirectory(), 'templates/component.mustache');
         const template = requireText(pathToTemplate, require);
-        const result = Mustache.render(template, {componentName: args.componentName});
-        execute('mkdir', ['-p', directoryPath]).then(() => {
-            writeToFile(directoryPath + '/' + componentName, result).then(() => {
+        const result = Mustache.render(template, { componentName: args.componentName });
+
+        mkdir(directoryPath)
+            .then(() => writeToFile(path.join(directoryPath, componentName), result))
+            .then(() => {
                 console.log(chalk.green('Component generated!'));
-                console.log(chalk.bold('PATH: ' + directoryPath + '/' + componentName));
-            }, (err) => {
-                console.log(chalk.red(err));
+                console.log(chalk.bold('PATH: ' + path.join(directoryPath, componentName)));
+            })
+            .catch((err: Error) => {
+                return console.log(chalk.red(err.message));
             });
-        });
 
     }
 }
