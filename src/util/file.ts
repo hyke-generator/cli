8eb405de0830f0a246c48095f10da4708ba92636
Luka Klacar <l.klacar@vegaitsourcing.rs>
2018-10-01 15:47:26 +0200

    #3 - Minor changes

--- src/util/file.ts
+++ src/util/file.ts
@@ -1,18 +1,26 @@
-import * as fs from "fs";
-import { PathLike } from "fs";
+import * as fs from 'fs';
+import { PathLike } from 'fs';
+import { execute } from './execute';
+import { ChildProcess } from 'child_process';
 
 export function writeToFile(path: PathLike, data: any): Promise<void> {
     return new Promise<void>((resolve, reject) => {
         if (fs.existsSync(path)) {
-            reject('Already exists!');
+            reject(new Error('Already exists!'));
         }
         fs.writeFile(path, data, (err) => {
-            if(err) {
-                console.log(err);
-                reject();
-                return;
+            if (err) {
+                reject(new Error(`Couldn't write to file ${path}`));
+            } else {
+                resolve();
             }
-            resolve();
         });
     });
+}
+
+export function mkdir(directoryPath: string): Promise<ChildProcess> {
+    return execute('mkdir', ['-p', directoryPath])
+        .catch(() => {
+            throw new Error(`Couldn't create ${directoryPath}`);
+        });
 }
