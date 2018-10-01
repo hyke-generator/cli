31b73d7da95d952de6557e875b3a8567fc59a1d1
Luka Klacar <l.klacar@vegaitsourcing.rs>
2018-10-01 12:59:12 +0200

    We need ./dist

---
+++ dist/generators/core/BaseGenerator.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class BaseGenerator {
+}
+exports.default = BaseGenerator;
+//# sourceMappingURL=BaseGenerator.js.map
---
+++ dist/generators/new/NewApplicationGenerator.js
@@ -0,0 +1,33 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const BaseGenerator_1 = __importDefault(require("../core/BaseGenerator"));
+const execute_1 = require("../../util/execute");
+// @ts-ignore
+const chalk_animation_1 = __importDefault(require("chalk-animation"));
+// @ts-ignore
+const chalk_1 = __importDefault(require("chalk"));
+const path = __importStar(require("path"));
+class NewApplicationGenerator extends BaseGenerator_1.default {
+    generate(args) {
+        chalk_animation_1.default.rainbow('Generating React Native application. Please wait...');
+        const reactNativeCliPath = path.join(__dirname, '..', '..', '..', 'node_modules', 'react-native-cli', 'index.js');
+        execute_1.execute('node', [reactNativeCliPath, 'init', args.appName, '--template', 'hike'])
+            .then(() => execute_1.execute('node', [`${args.appName}/setup.js`]))
+            .then(() => console.log(chalk_1.default.green('Application successfully generated.')))
+            .catch(() => {
+            console.log(chalk_1.default.red('Error while generating React Native application.'));
+        });
+    }
+}
+exports.default = NewApplicationGenerator;
+//# sourceMappingURL=NewApplicationGenerator.js.map
